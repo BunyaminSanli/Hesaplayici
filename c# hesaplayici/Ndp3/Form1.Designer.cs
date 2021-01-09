b5ed12ee9dc020df5d7d23e75ae3e1f0bca865cc
BunyaminSanli <60830225+BunyaminSanli@users.noreply.github.com>
2021-01-09 15:30:51 +0300

    hesaplayici

---
+++ c# hesaplayici/Ndp3/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Ndp3
+{
+    partial class Hesaplayici
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formulBox = new System.Windows.Forms.TextBox();
+            this.buttonHesapla = new System.Windows.Forms.Button();
+            this.sonucBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // formulBox
+            // 
+            this.formulBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.formulBox.Location = new System.Drawing.Point(12, 33);
+            this.formulBox.Name = "formulBox";
+            this.formulBox.Size = new System.Drawing.Size(229, 24);
+            this.formulBox.TabIndex = 0;
+            this.formulBox.TextChanged += new System.EventHandler(this.formulBox_TextChanged);
+            // 
+            // buttonHesapla
+            // 
+            this.buttonHesapla.Location = new System.Drawing.Point(12, 84);
+            this.buttonHesapla.Name = "buttonHesapla";
+            this.buttonHesapla.Size = new System.Drawing.Size(229, 24);
+            this.buttonHesapla.TabIndex = 1;
+            this.buttonHesapla.Text = "HESAPLA";
+            this.buttonHesapla.UseVisualStyleBackColor = true;
+            this.buttonHesapla.Click += new System.EventHandler(this.buttonHesapla_Click);
+            // 
+            // sonucBox
+            // 
+            this.sonucBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sonucBox.Location = new System.Drawing.Point(12, 135);
+            this.sonucBox.Name = "sonucBox";
+            this.sonucBox.ReadOnly = true;
+            this.sonucBox.Size = new System.Drawing.Size(229, 24);
+            this.sonucBox.TabIndex = 2;
+            this.sonucBox.TextChanged += new System.EventHandler(this.sonucBox_TextChanged);
+            // 
+            // Hesaplayici
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 217);
+            this.Controls.Add(this.sonucBox);
+            this.Controls.Add(this.buttonHesapla);
+            this.Controls.Add(this.formulBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Hesaplayici";
+            this.Text = "HESAPLAYICI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox formulBox;
+        private System.Windows.Forms.Button buttonHesapla;
+        private System.Windows.Forms.TextBox sonucBox;
+    }
+}
+
---
+++ c# hesaplayici/Ndp3/Form1.cs
@@ -0,0 +1,129 @@
+﻿/****************************************************************************
+**					          SAKARYA ÜNİVERSİTESİ
+**				     BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				           BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				          NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					          2018-2019 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 3
+**				ÖĞRENCİ ADI............: BÜNYAMİN ŞANLI
+**				ÖĞRENCİ NUMARASI.......: B181210040
+**                         DERSİN ALINDIĞI GRUP...: 1B
+****************************************************************************/
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Ndp3
+{
+    public partial class Hesaplayici : Form
+    {
+        public Hesaplayici()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHesapla_Click(object sender, EventArgs e)
+        {
+            var nums = formulBox.Text.Split("/*-+".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            var otps = Regex.Split(formulBox.Text, @"\d", RegexOptions.None).Where(p => p != "").ToArray<string>();
+            List<string> sayilar = new List<string>();
+            foreach (string sayi in nums)
+            {
+                sayilar.Add(sayi);
+            }
+
+            List<string> operators = new List<string>();
+            foreach (string otp in otps)
+            {
+                operators.Add(otp);
+            }
+
+            double ilksayi = double.Parse(sayilar[0]);
+            double deger = 0;
+            for (int i = 0; i < operators.Count; i++)
+            {
+                switch (operators[i])
+                {
+                    case "*":
+                        if (i + 1 < sayilar.Count)
+                        {
+                            deger = (double.Parse(sayilar[i]) * double.Parse(sayilar[i + 1]));
+                            sayilar[i] = Convert.ToString(deger);
+                            sayilar.RemoveAt(i + 1);
+                            operators.Remove("*");
+                            i = -1;
+                        }
+                        break;
+                    case "/":
+                        if (i + 1 < sayilar.Count)
+                        {
+                            deger = (double.Parse(sayilar[i]) / double.Parse(sayilar[i + 1]));
+                            sayilar[i] = Convert.ToString(deger);
+                            sayilar.RemoveAt(i + 1);
+                            operators.Remove("/");
+                            i = -1;
+                        }
+                        break;
+                }
+            }
+
+            double toplam = 0;
+            for (int i = 0; i < operators.Count; i++)
+            {
+                switch (operators[i])
+                {
+                    case "+":
+                        if (i + 1 < sayilar.Count)
+                        {
+                            deger = (double.Parse(sayilar[i]) + double.Parse(sayilar[i + 1]));
+                            sayilar[i] = Convert.ToString(deger);
+                            sayilar.RemoveAt(i + 1);
+                            operators.Remove("+");
+                            i = -1;
+                        }
+                        break;
+                    case "-":
+                        if (i + 1 < sayilar.Count)
+                        {
+                            deger = (double.Parse(sayilar[i]) - double.Parse(sayilar[i + 1]));
+                            sayilar[i] = Convert.ToString(deger);
+                            sayilar.RemoveAt(i + 1);
+                            operators.Remove("-");
+                            i = -1;
+                        }
+                        break;
+                }
+            }
+
+            sonucBox.Text = sayilar[0];
+        }
+
+        private void formulBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sonucBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
+
+
